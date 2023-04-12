1dd08e056d0809914f8cfa40d1c5bca6726caa2e
yazidi112 <yazidi.imran@gmail.com>
2023-04-12 13:37:50 +0000

    update db.py

---
+++ db.py
@@ -0,0 +1,42 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="wt"
+)
+
+mycursor = mydb.cursor()
+
+class Site:
+    @staticmethod
+    def insert(site,case1,case2,case3,case4,case5):
+        sql = "INSERT INTO sites  VALUES (null, %s,%s,%s,%s,%s,%s)"
+        val = (site,case1,case2,case3,case4,case5)
+        mycursor.execute(sql, val)
+        mydb.commit()
+        print(mycursor.rowcount, "Site inserted.")
+    
+    @staticmethod
+    def clear():
+        sql = "TRUNCATE  sites"
+        mycursor.execute(sql)
+        mydb.commit()
+        print(mycursor.rowcount, "Table sites emptied.")
+
+class Request:
+    @staticmethod
+    def insert(site,url,typ,method,status,location,sharing_technique):
+        sql = "INSERT INTO requests  VALUES (null, %s,%s,%s,%s,%s,%s,%s)"
+        val = (site,url,typ,method,status,location,sharing_technique)
+        mycursor.execute(sql, val)
+        mydb.commit()
+        print(mycursor.rowcount, "Request inserted.")
+    
+    @staticmethod
+    def clear():
+        sql = "TRUNCATE  requests"
+        mycursor.execute(sql)
+        mydb.commit()
+        print(mycursor.rowcount, "Table Requests emptied.")
